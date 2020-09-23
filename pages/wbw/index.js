9151e753521d596aca3af6077a22060004b608f3
JonasVervloet <vervloetjonas@hotmail.com>
2020-09-23 19:07:56 +0200

    wbw home page

---
+++ pages/wbw/index.js
@@ -0,0 +1,9 @@
+export default function WbwHome() {
+    return (
+        <div>
+            <h1>Villa Vaag</h1>
+            <h2>Wie Betaald Wat?!</h2>
+            <button>click here to login-in</button>
+        </div>
+    )
+} 
