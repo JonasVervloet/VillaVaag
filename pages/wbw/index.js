b60130c7585dbbb691dede9c6fef855a88f143e5
JonasVervloet <vervloetjonas@hotmail.com>
2020-09-24 20:46:48 +0200

    create basic wbw landing page

--- pages/wbw/index.js
+++ pages/wbw/index.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export default function WbwHome() {
     return (
         <div>
             <h1>Villa Vaag</h1>
             <h2>Wie Betaald Wat?!</h2>
-            <button>click here to login-in</button>
+            <Link href="/">
+                <a>click here to login-in</a>
+            </Link>
         </div>
     )
 } 
