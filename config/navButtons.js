23eaeb3361d1e79e3d5abcb2f75f191b9ede4371
JonasVervloet <vervloetjonas@hotmail.com>
2020-11-16 21:33:26 +0100

    WBW Navbar buttons

---
+++ config/navButtons.js
@@ -0,0 +1,16 @@
+const navButtons = [
+    {
+        label: 'My Groups',
+        path: "login"
+    },
+    {
+        label: 'Shopping List',
+        path: 'login'
+    },
+    {
+        label: 'Profile',
+        path: 'login'
+    }
+]
+
+export default navButtons;
