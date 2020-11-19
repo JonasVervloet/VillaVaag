eadd643dbecb7d03c3ce50323a82179041ce0a9a
JonasVervloet <vervloetjonas@hotmail.com>
2020-11-19 19:50:32 +0100

    add isLogged reducer

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+
+import '../styles/globals.css';
+import rootReducer from '../reducers';
+
+const store = createStore(rootReducer);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
