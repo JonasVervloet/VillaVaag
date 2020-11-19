c49fa7a05012e7a0c83f1db4fdaad65b35861e85
JonasVervloet <vervloetjonas@hotmail.com>
2020-11-19 20:12:03 +0100

    basic landing page

--- pages/wbw/index.js
+++ pages/wbw/index.js
@@ -1,16 +1,31 @@
 import Link from 'next/link';
 
-export default function WbwHome() {
+import styles from '../../styles/WBWHome.module.css'
+
+export default function WBWHome() {
     return (
-        <div>
-            <h1>
+        <div className={
+            styles.landingPageContainer
+            + " white-background"
+        }>
+            <h1 className={
+                styles.titleText
+                + " dark-text-color"
+            }>
                 Villa Vaag
             </h1>
-            <h2>
-                Wie Betaald Wat?!
-            </h2>
+            <h1 className={
+                styles.titleText
+                + " dark-text-color"
+            }>
+                WBW
+            </h1>
             <Link href="wbw/groups">
-                <a>
+                <a className={
+                    styles.loginText
+                    + " dark-text-color"
+                    + " dark-text-hover"
+                }>
                     click here to login-in
                 </a>
             </Link>
